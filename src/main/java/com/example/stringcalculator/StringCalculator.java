5355f4d736f5671ac9cb884681cac863a1dfe835
Patrik Fallqvist Magnusson <patrik.fallqvist@gmail.com>
2022-01-16 18:39:20 +0100

    Add test for one number

--- src/main/java/com/example/stringcalculator/StringCalculator.java
+++ src/main/java/com/example/stringcalculator/StringCalculator.java
@@ -1,8 +1,17 @@
 package com.example.stringcalculator;
 
+import java.util.Scanner;
+
 public class StringCalculator {
 
-    public int add(String s) {
-        return 0;
+    public int add(String numbers) {
+
+        if (numbers.equals("")) {
+            return 0;
+        } else {
+            return Integer.parseInt(numbers);
+        }
+
+
     }
 }
--- src/test/java/com/example/stringcalculator/StringCalculatorTest.java
+++ src/test/java/com/example/stringcalculator/StringCalculatorTest.java
@@ -5,13 +5,19 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringCalculatorTest {
+    StringCalculator stringCalculator = new StringCalculator();
 
     @Test
     void addEmptyStingShouldReturnZero() {
-        StringCalculator stringCalculator = new StringCalculator();
-
         int result = stringCalculator.add("");
 
         assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    void addOneNumberShouldReturnOne() {
+        int result = stringCalculator.add("1");
+
+        assertThat(result).isEqualTo(1);
     }
 }
