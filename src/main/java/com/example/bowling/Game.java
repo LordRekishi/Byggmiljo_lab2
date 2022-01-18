972f81aa79920027945f805a8c08967e773245e5
Patrik Fallqvist Magnusson <patrik.fallqvist@gmail.com>
2022-01-18 20:51:07 +0100

    Add Refactor of Bowling

--- src/main/java/com/example/bowling/Game.java
+++ src/main/java/com/example/bowling/Game.java
@@ -1,6 +1,8 @@
 package com.example.bowling;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 
 public class Game {
@@ -18,38 +20,38 @@ public class Game {
             if (rollCounter == 19 && rolls.get(rolls.size() - 1) + pins == 10) {
                 MAX_ROLLS++;
             }
-            if (rolls.size() > 1) {
-                strike(pins);
-                spare(pins);
-            }
-            if (rollCounter != 20) {
-                rolls.add(pins);
-            }
+            strike(pins);
+
+            rolls.add(pins);
             rollCounter++;
         }
     }
 
     private void strike(int pins) {
-        if (rolls.get(rolls.size() - 2) == MAX_PINS && rollCounter == 20) {
-          score -= (rolls.get(rolls.size() - 1) + pins);
-        }
-        if (rolls.get(rolls.size() - 2) == MAX_PINS) {
-            score += (rolls.get(rolls.size() - 1) + pins);
+        if (pins == MAX_PINS) {
             rollCounter++;
         }
     }
 
-    private void spare(int pins) {
-        if (rollCounter % 2 == 0) {
-            if (rolls.get(rolls.size() - 2) + rolls.get(rolls.size() - 1) == MAX_PINS) {
-                score += pins;
-            }
-        }
-    }
-
     public int score() {
-        for (Integer roll : rolls) {
-            score += roll;
+
+        int count = 0;
+        for (int i = 0; i < rollCounter; i++) {
+            if (count < 20) {
+                score += rolls.get(i);
+            }
+
+            if (count % 2 == 0) {
+                if (rolls.get(i) == 10) {
+                    score += rolls.get(i + 1) + rolls.get(i + 2);
+                    count++;
+                }
+
+                if (rolls.get(i) + rolls.get(i + 1) == 10) {
+                    score += rolls.get(i + 2);
+                }
+            }
+            count++;
         }
         return score;
     }
