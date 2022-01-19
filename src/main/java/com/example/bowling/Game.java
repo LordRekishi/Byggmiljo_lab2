bb6d245f8946f52c6942b4fcfc4ac7709f471365
Patrik Fallqvist Magnusson <patrik.fallqvist@gmail.com>
2022-01-19 19:16:58 +0100

    Add refactor of Game class

--- src/main/java/com/example/bowling/Game.java
+++ src/main/java/com/example/bowling/Game.java
@@ -4,9 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
-    private List<Frame> frames = new ArrayList<>();
+    private final List<Frame> frames = new ArrayList<>();
     private final int MAX_PINS = 10;
-    private final int MAX_FRAMES = 10;
     private int score;
     private int rollCounter;
 
@@ -14,35 +13,19 @@ public class Game {
         Frame frame;
 
         if (pins <= MAX_PINS) {
+            int MAX_FRAMES = 10;
+
             if (rollCounter == 0 && frames.size() < MAX_FRAMES) {
                 frame = new Frame();
                 frame.setFirstRoll(pins);
 
-                if (pins == MAX_PINS) {
-                    if (frames.size() == 9) {
-                        frame.setStrike(true);
-                        rollCounter++;
-                    } else {
-                        frame.setStrike(true);
-                        rollCounter = 0;
-                    }
+                if (isStrike(pins)) {
+                    setStrike(frame);
                 } else {
                     rollCounter++;
                 }
 
-                if (frames.size() > 0) {
-                    if (getLastFrame().isStrike()) {
-                        getLastFrame().addFrameScore(frame.getFirstRoll());
-                    }
-                    if (getLastFrame().isSpare()) {
-                        getLastFrame().addFrameScore(frame.getFirstRoll());
-                    }
-                }
-                if (frames.size() > 1) {
-                    if (getSecondLastFrame().isStrike() && getLastFrame().isStrike()) {
-                        getSecondLastFrame().addFrameScore(frame.getFirstRoll());
-                    }
-                }
+                addScoreForStrikesAndSparesInEarlierFrames(frame);
 
                 frame.addFrameScore(pins);
                 frames.add(frame);
@@ -50,30 +33,82 @@ public class Game {
             } else if (rollCounter == 1 && frames.size() <= MAX_FRAMES) {
                 frame = getLastFrame();
                 frame.setSecondRoll(pins);
-
                 frame.addFrameScore(pins);
 
-                if (frame.getFirstRoll() + frame.getSecondRoll() == MAX_PINS) {
-                    frame.setSpare(true);
-                }
+                checkIfSpare(frame);
+
                 rollCounter = 0;
 
-                if (frames.size() > 1) {
-                    if (getSecondLastFrame().isStrike()) {
-                        getSecondLastFrame().addFrameScore(frame.getSecondRoll());
-                    }
-                }
+                addScoreForStrikeInEarlierFrame(frame);
+                changeRollCounterForExtraRollForStrikeOrSpareInFrameTen(frame);
 
-                if (frame.isStrike() || frame.isSpare() && frames.size() == 10) {
-                    rollCounter = 2;
-                }
             } else if (rollCounter == 2 && frames.size() <= MAX_FRAMES) {
-                frame = getLastFrame();
-                frame.setExtraRoll(pins);
-                frame.addFrameScore(pins);
+                extraRollOnFrameTen(pins);
             }
         }
 
+    }
+
+    private boolean isStrike(int pins) {
+        return pins == MAX_PINS;
+    }
+
+    private void setStrike(Frame frame) {
+        if (frames.size() == 9) {
+            frame.setStrike(true);
+            rollCounter++;
+        } else {
+            frame.setStrike(true);
+            rollCounter = 0;
+        }
+    }
+
+    private void addScoreForStrikesAndSparesInEarlierFrames(Frame frame) {
+        if (frames.size() > 0) {
+            addScoreInLastFrameForStrikeOrSpare(frame);
+        }
+        if (frames.size() > 1) {
+            addScoreInSecondLastFrameForStrikeIfLastFrameWasAlsoStrike(frame);
+        }
+    }
+
+    private void addScoreInLastFrameForStrikeOrSpare(Frame frame) {
+        if (getLastFrame().isStrike()) {
+            getLastFrame().addFrameScore(frame.getFirstRoll());
+        }
+        if (getLastFrame().isSpare()) {
+            getLastFrame().addFrameScore(frame.getFirstRoll());
+        }
+    }
+
+    private void addScoreInSecondLastFrameForStrikeIfLastFrameWasAlsoStrike(Frame frame) {
+        if (getSecondLastFrame().isStrike() && getLastFrame().isStrike()) {
+            getSecondLastFrame().addFrameScore(frame.getFirstRoll());
+        }
+    }
+
+    private void checkIfSpare(Frame frame) {
+        if (frame.getFirstRoll() + frame.getSecondRoll() == MAX_PINS) {
+            frame.setSpare(true);
+        }
+    }
+
+    private void addScoreForStrikeInEarlierFrame(Frame frame) {
+        if (frames.size() > 1) {
+            if (getSecondLastFrame().isStrike()) {
+                getSecondLastFrame().addFrameScore(frame.getSecondRoll());
+            }
+        }
+    }
+
+    private void changeRollCounterForExtraRollForStrikeOrSpareInFrameTen(Frame frame) {
+        if (frame.isStrike() || frame.isSpare() && frames.size() == 10) {
+            rollCounter = 2;
+        }
+    }
+
+    private void extraRollOnFrameTen(int pins) {
+        getLastFrame().setExtraRoll(pins).addFrameScore(pins);
     }
 
     private Frame getLastFrame() {
