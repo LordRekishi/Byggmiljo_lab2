f7b8fb1cd92441096972fc49b388922fb0e7d5c2
Patrik Fallqvist Magnusson <patrik.fallqvist@gmail.com>
2022-01-21 14:13:39 +0100

    Add refactor of String Calculator class

--- src/main/java/com/example/stringcalculator/StringCalculator.java
+++ src/main/java/com/example/stringcalculator/StringCalculator.java
@@ -12,39 +12,45 @@ public class StringCalculator {
         if (numbers.equals("")) {
             return 0;
         }
-        if (numbers.startsWith("//[")) {
-            Pattern pattern = Pattern.compile("\\[.*?]");
-            Matcher matcher = pattern.matcher(numbers);
 
-            StringBuilder stringBuilder = new StringBuilder();
-
-            while (matcher.find()) {
-                stringBuilder.append("\\Q")
-                        .append(matcher.group(), 1, matcher.group().length() - 1)
-                        .append("\\E")
-                        .append("|");
-            }
-            stringBuilder.append("\n");
-
-            delimiter = stringBuilder.toString();
-        } else if (numbers.startsWith("//") && !numbers.startsWith("//[")) {
-            delimiter = numbers.charAt(2) + "|\n";
-        }
-
-        if (numbers.contains("-")) {
-            StringBuilder negatives = getStringOfNegatives(numbers, delimiter);
-            throw new RuntimeException("Negatives " + negatives + " not allowed");
-        }
+        delimiter = setDelimiterIfNotDefault(numbers, delimiter);
+        checkForNegatives(numbers, delimiter);
 
         return getSum(numbers, delimiter);
     }
 
-    private int getSum(String numbers, String delimiter) {
-        return Stream.of(numbers.split(delimiter))
-                .filter(this::isNumeric)
-                .mapToInt(Integer::parseInt)
-                .filter(value -> value <= 1000)
-                .sum();
+    private String setDelimiterIfNotDefault(String numbers, String delimiter) {
+        if (numbers.startsWith("//[")) {
+            Pattern pattern = Pattern.compile("\\[.*?]");
+            Matcher matcher = pattern.matcher(numbers);
+            delimiter = buildDelimiterString(matcher);
+        } else if (numbers.startsWith("//")) {
+            delimiter = numbers.charAt(2) + "|\n";
+        }
+        return delimiter;
+    }
+
+    private String buildDelimiterString(Matcher matcher) {
+        String delimiter;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        while (matcher.find()) {
+            stringBuilder.append("\\Q")
+                    .append(matcher.group(), 1, matcher.group().length() - 1)
+                    .append("\\E")
+                    .append("|");
+        }
+        stringBuilder.append("\n");
+
+        delimiter = stringBuilder.toString();
+        return delimiter;
+    }
+
+    private void checkForNegatives(String numbers, String delimiter) {
+        if (numbers.contains("-")) {
+            StringBuilder negatives = getStringOfNegatives(numbers, delimiter);
+            throw new RuntimeException("Negatives " + negatives + " not allowed");
+        }
     }
 
     private StringBuilder getStringOfNegatives(String numbers, String delimiter) {
@@ -57,6 +63,14 @@ public class StringCalculator {
         negatives.append(")");
 
         return negatives;
+    }
+
+    private int getSum(String numbers, String delimiter) {
+        return Stream.of(numbers.split(delimiter))
+                .filter(this::isNumeric)
+                .mapToInt(Integer::parseInt)
+                .filter(value -> value <= 1000)
+                .sum();
     }
 
     private boolean isNumeric(String input) {
