b9afad1b4fb8df915d8e57c6870dac0cc718fb27
Patrik Fallqvist Magnusson <patrik.fallqvist@gmail.com>
2022-01-23 18:21:18 +0100

    Add save method test of unique employee

--- src/test/java/com/example/EmployeeRepositoryInMemoryTest.java
+++ src/test/java/com/example/EmployeeRepositoryInMemoryTest.java
@@ -3,16 +3,18 @@ package com.example;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class EmployeeRepositoryInMemoryTest {
-    EmployeeRepository employeeRepository = new EmployeeRepositoryInMemory(List.of(
+    List<Employee> employees = new ArrayList<>(Arrays.asList(
             new Employee("1", 10000),
             new Employee("2", 30000),
-            new Employee("3", 40000)
-    ));
+            new Employee("3", 40000)));
+    EmployeeRepository employeeRepository = new EmployeeRepositoryInMemory(employees);
 
     @Test
     @DisplayName("Test Find All Method should return Three")
@@ -20,6 +22,15 @@ class EmployeeRepositoryInMemoryTest {
         var result = employeeRepository.findAll().size();
 
         assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("Test Save Method with new employee should return Four")
+    void testSaveMethodWithNewEmployeeShouldReturnFour() {
+        employeeRepository.save(new Employee("4", 2000));
+        var result = employeeRepository.findAll().size();
+
+        assertThat(result).isEqualTo(4);
     }
 
 }
